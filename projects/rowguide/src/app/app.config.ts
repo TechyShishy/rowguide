5e8ead77a0e13290da1278c84859265b6b5c9d96
Kaylee Hirsch <emperorshishire@gmail.com>
2024-02-14 13:52:50 -0800

    Remove SSR configuration

--- projects/rowguide/src/app/app.config.ts
+++ projects/rowguide/src/app/app.config.ts
@@ -2,13 +2,11 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(),
     importProvidersFrom(LoggerModule.forRoot({ level: NgxLoggerLevel.DEBUG })),
   ],
 };
