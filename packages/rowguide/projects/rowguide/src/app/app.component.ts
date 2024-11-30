b3ff0b9684fc073dfb2bdab1d45c994e936ddbd8
Kaylee Hirsch <emperorshishire@gmail.com>
2024-11-30 09:12:57 -0800

    Add project name to project tab

--- packages/rowguide/projects/rowguide/src/app/app.component.ts
+++ packages/rowguide/projects/rowguide/src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatTabGroup, MatTab } from '@angular/material/tabs';
 import { ProjectSelectorComponent } from './project-selector/project-selector.component';
 import { ProjectComponent } from './project/project.component';
 import { SettingsComponent } from './settings/settings.component';
+import { ProjectService } from './project.service';
+import { Project } from './project';
 
 @Component({
   selector: 'app-root',
@@ -21,4 +23,16 @@ import { SettingsComponent } from './settings/settings.component';
 })
 export class AppComponent {
   title = 'rowguide';
+  project!: Project;
+
+  constructor(
+    private projectService: ProjectService,
+    private cdr: ChangeDetectorRef
+  ) {
+    this.project = this.projectService.project;
+    this.projectService.ready.subscribe((ready) => {
+      this.project = this.projectService.project;
+      this.cdr.detectChanges();
+    });
+  }
 }
