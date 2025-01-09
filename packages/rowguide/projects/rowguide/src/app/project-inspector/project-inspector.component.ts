88ec3dd8287d9e1f2f77d5581bf03973e3055e59
Kaylee Hirsch <emperorshishire@gmail.com>
2025-01-09 10:03:19 -0800

    Update project-inspector to include more info

--- packages/rowguide/projects/rowguide/src/app/project-inspector/project-inspector.component.ts
+++ packages/rowguide/projects/rowguide/src/app/project-inspector/project-inspector.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FlamService } from '../flam.service';
 import { FLAM } from '../flam';
 import { CommonModule, NgFor, NgForOf } from '@angular/common';
@@ -6,20 +6,26 @@ import { FLAMRow } from '../flamrow';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { SettingsService } from '../settings.service';
+import { ProjectService } from '../project.service';
+import { NGXLogger } from 'ngx-logger';
 
 @Component({
   selector: 'app-project-inspector',
   standalone: true,
   imports: [CommonModule, MatCardModule, MatListModule],
   templateUrl: './project-inspector.component.html',
-  styleUrl: './project-inspector.component.scss',
+  styleUrls: ['./project-inspector.component.scss'],
 })
-export class ProjectInspectorComponent {
+export class ProjectInspectorComponent implements OnInit {
   flam: Array<FLAMRow> = [];
+
   constructor(
     public flamService: FlamService,
-    public settingsService: SettingsService
+    public settingsService: SettingsService,
+    public projectService: ProjectService,
+    public logger: NGXLogger
   ) {}
+
   ngOnInit() {
     this.flamService.inititalizeFLAM(true);
     this.flam = Object.values(this.flamService.flam);
